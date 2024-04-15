a0c1f51cc509ff54312be1b4cfad216d6c9b9e6a
nastya <eg.nastyaa@gmail.com>
2024-04-15 18:16:21 +0300

    Мой первый коммит!

---
+++ domains_selectors.py
@@ -0,0 +1,12 @@
+DOMAIN_SELECTOR = {
+    'www.wildberries.ru': 'ins[class="price-block__final-price wallet"]',
+    'market.yandex.ru': 'span[data-auto="snippet-price-old"]',
+    'www.mvideo.ru': 'span[_ngcontent-serverapp-c1106472620]',
+    'aliexpress.ru': 'div[class="HazeProductPrice_SnowPrice__mainS__1jbkl"]',
+    'pitergsm.ru': 'span[class="main-detail-price"]',
+    'www.citilink.ru': 'span[class="e1j9birj0 e106ikdt0 app-catalog-8hy98m e1gjr6xo0"]',
+    'megamarket.ru': 'span[class="sales-block-offer-price__price-final"]'
+}
+DOMAIN_SELECTOR_ADD = {
+    'market.yandex.ru': 'h3[class="_3oild _6tyDq u1R1k sslMJ"]'
+}
