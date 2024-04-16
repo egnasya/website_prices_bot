8d2e95c534b2ee51971d7acb9efe5188f46bcd8e
Anastasiia <eg.nastyaa@gmail.com>
2024-04-16 14:36:07 +0300

    создан хендлер, обрабатывающий команду "start"

--- bot.py
+++ bot.py
@@ -1,34 +1,23 @@
-import requests
-import time
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command, CommandStart
+from aiogram.types import Message
 from my_token import TOKEN
 
 
-API_URL = 'https://api.telegram.org/bot'
-API_CATS_URL = 'https://api.thecatapi.com/v1/images/search'
-BOT_TOKEN = TOKEN
-ERROR_TEXT = 'здесь должна быть картинка с котиком :('
-MAX_COUNTER = 100
+# создание объектов бота и диспечера
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
 
-offset = -2
-counter = 0
-chat_id : int
-cat_response : requests.Response
 
-while counter < MAX_COUNTER:
-    print('attempt = ', counter)
-    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
+# хендлер, который срабатывает на команду 'start'
+@dp.message(Command(commands=['start']))
+async def process_start_command(message: Message):
+    await message.answer('Привет! Я бот, который может отслеживать цены на товары из различных оналйн магазинов.\n\n'
+                         'Для того, чтобы начать отслеживать цену на товар, выбери в меню команду "Добавить ссылку на '
+                         'товар" и отправь мне ссылку на этот товар.\nЕсли ты захочешь прекраить отслеживание, то '
+                         'выбери в меню пункт "Перестать отслеживать товар" и выбери товар из списка.\n'
+                         'Если окажется, что в нашей базе нет сайта, цену с которого ты хочешь отслеживать - выбери '
+                         'команду "Обратная связь" и напиши название сайта.\n\nПриятного использования :)')
 
-    if updates['result']:
-        for update in updates['result']:
-            offset = update['update_id']
-            chat_id = update['message']['from']['id']
-            cat_response = requests.get(API_CATS_URL)
-            if cat_response.status_code == 200:
-                cat_link = cat_response.json()[0]['url']
-                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}').json()
-            else:
-                response = requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}').json()
-
-    time.sleep(1)
-    counter += 1
-
+if __name__ == '__main__':
+    dp.run_polling(bot)
