5869dd58d8b1dda27fc811b32739eef2934d277b
Anastasiia <eg.nastyaa@gmail.com>
2024-04-16 22:33:37 +0300

    в функцию get_price добавлена обработка исключений и извлечение наименования товара

--- scraper.py
+++ scraper.py
@@ -1,5 +1,10 @@
 from urllib.parse import urlparse
+from selenium.common import WebDriverException, NoSuchWindowException, TimeoutException, StaleElementReferenceException, \
+    NoSuchElementException
+from selenium.webdriver.common.by import By
+
 from domains_selectors import DOMAIN_SELECTOR, DOMAIN_SELECTOR_ADD
+from selenium import webdriver
 
 
 def website_recognition(url):
@@ -12,5 +17,37 @@ def website_recognition(url):
 
 
 def get_price(site_url, key):
-    # тут будет скрапер
+    global driver
+
+    try:
+        # options = webdriver.ChromeOptions()
+        driver = webdriver.Chrome()
+        driver.implicitly_wait(15)
+        driver.get(site_url)
+    except TimeoutException:
+        print('Время ожидания операции истекло.')
+    except NoSuchWindowException:
+        print('Окно браузера не найдено.')
+    except Exception as e:
+        print(f'Произошла непредвиденная ошибка: {e}')
+
+    else:
+        print('пока все кул')
+        name_product = None
+        price = None
+
+        try:
+            name_product = driver.find_element(By.TAG_NAME, 'h1')
+            print(name_product.text)
+        except NoSuchElementException:
+            print('Элемент с тегом "h1" не найден на странице.')
+        except Exception as e:
+            print(f'Произошла непредвиденная ошибка: {e}')
+
+    finally:
+        driver.quit()
+
     return 0
+
+
+get_price('https://www.wildberries.ru/catalog/181047832/detail.aspx?targetUrl=MS&size=29908097', 'www.wildberries.ru')
