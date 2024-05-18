f831106eeb1d7f88fc3620422ac78c23e87bc91c
Anastasiia <eg.nastyaa@gmail.com>
2024-05-18 19:53:06 +0300

    я ща плакать уже буду

--- scraper.py
+++ scraper.py
@@ -1,25 +1,20 @@
 import re
+import asyncio
 from urllib.parse import urlparse
 import requests
 import validators
-from selenium.common import NoSuchWindowException, TimeoutException, NoSuchElementException
+from selenium.common.exceptions import NoSuchWindowException, TimeoutException, NoSuchElementException
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium import webdriver
-from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from xvfbwrapper import Xvfb
+from selenium import webdriver
 
 from domains_selectors import DOMAIN_SELECTOR, DOMAIN_SELECTOR_ADD
 import manipulation_db
 
 
-vdisplay = Xvfb()
-vdisplay.start()
-
-
 async def website_recognition(text, user_id):
-
     valid = validators.url(text)
 
     if not valid:
@@ -28,7 +23,7 @@ async def website_recognition(text, user_id):
         try:
             response = requests.get(text)
             if response.status_code != 200:
-                return 'Произошла ошибка при загрузке страницы   :('
+                return 'Произошла ошибка при загрузке страницы :('
 
             url = text
             domain_url = urlparse(url).netloc
@@ -42,59 +37,52 @@ async def website_recognition(text, user_id):
 
 
 async def get_price(site_url, key, user_id):
-    global driver, price, name_product
+    loop = asyncio.get_running_loop()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-setuid-sandbox")
+    chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument('--disable-dev-shm-usage')  # Использование /dev/shm как хранилище
+    chrome_options.add_argument('--disable-gpu')  # Отключение GPU
+    chrome_options.add_argument('--disable-software-rasterizer')  # Отключение программного растеризатора
+    chrome_options.add_argument('--disable-extensions')
+    chrome_options.add_argument('--disable-in-process-stack-traces')
+    chrome_options.add_argument('--disable-logging')
+    chrome_options.add_argument('--log-level=3')
+    chrome_options.add_argument('--output=/dev/null')
+
+    service = Service(executable_path='/home/nastya/website_prices_bot/chromedriver')
+    driver = None
 
     try:
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--disable-setuid-sandbox")
-        chrome_options.add_argument('--remote-debugging-pipe')
-        chrome_options.add_argument("--headless=new")
-        chrome_options.add_argument('--disable-dev-shm-usage')  # Использование /dev/shm как хранилище
-        chrome_options.add_argument('--disable-gpu')  # Отключение GPU
-        chrome_options.add_argument('--disable-software-rasterizer')  # Отключение программного растеризатора
-        chrome_options.add_argument('--disable-dev-shm-usage')
-        chrome_options.add_argument('--disable-extensions')
-        chrome_options.add_argument('--disable-in-process-stack-traces')
-        chrome_options.add_argument('--disable-logging')
-        chrome_options.add_argument('--log-level=3')
-        chrome_options.add_argument('--output=/dev/null')
-        service = Service(executable_path='/home/nastya/website_prices_bot/chromedriver')
         driver = webdriver.Chrome(service=service, options=chrome_options)
         driver.implicitly_wait(3)
-        driver.get(site_url)
-    except TimeoutException:
-        print('Время ожидания операции истекло.')
-    except NoSuchWindowException:
-        print('Окно браузера не найдено.')
-    except Exception as e:
-        print(f'Произошла непредвиденная ошибка: {e}')
-    else:
+
+        await loop.run_in_executor(None, driver.get, site_url)
+
+        # Явное ожидание загрузки заголовка страницы
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'h1')))
+
         try:
-            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'h1')))
             name_product = driver.find_element(By.TAG_NAME, 'h1').text.strip()
         except NoSuchElementException:
             print('Элемент с тегом "h1" не найден на странице.')
-        except Exception as e:
-            print(f'Произошла непредвиденная ошибка: {e}')
+            name_product = 'Unknown Product'
 
-        price = None
+        price_elements = await loop.run_in_executor(None, driver.find_elements, By.CSS_SELECTOR, DOMAIN_SELECTOR.get(key))
+        if not price_elements:
+            price_elements = await loop.run_in_executor(None, driver.find_elements, By.CSS_SELECTOR, DOMAIN_SELECTOR_ADD.get(key))
 
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, DOMAIN_SELECTOR[key])))
-        price_elements = driver.find_elements(By.CSS_SELECTOR, DOMAIN_SELECTOR[key])
         if price_elements:
             price = price_elements[0].text
         else:
-            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, DOMAIN_SELECTOR_ADD[key])))
-            price_elements = driver.find_elements(By.CSS_SELECTOR, DOMAIN_SELECTOR_ADD[key])
-            if price_elements:
-                price = price_elements[0].text
-            else:
-                print('Элемент с css-селекторами из базы данных на странице не найдены.')
+            print('Элемент с css-селекторами из базы данных на странице не найдены.')
+            return 'Unknown Product', 'error', site_url
 
         if price:
             price = re.sub('[\u00A0|\u2009]', '', price)
             price = re.sub('[A-Za-zА-Яа-я:]+', '', price)
+            price = re.sub('₽.*₽', '', price).strip()
             price = re.sub('₽[0-9]+₽*', '', price).strip()
             if price.endswith('₽'):
                 price = price[:-1]
@@ -104,7 +92,9 @@ async def get_price(site_url, key, user_id):
             return name_product, clean_price, site_url
         else:
             return name_product, 'error', site_url
-
+    except Exception as e:
+        print(f'Произошла непредвиденная ошибка: {e}')
+        return 'Unknown Product', 'error', site_url
     finally:
-        driver.quit()
-        vdisplay.stop()
+        if driver:
+            driver.quit()
